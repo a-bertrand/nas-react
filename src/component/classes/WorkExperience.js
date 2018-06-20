a17b3bd9f410e1e47a4d6e12973502036b64d9fb
a-bertrand <antho.bertrand101@gmail.com>
2018-06-20 16:04:25 +0200

    update

--- src/component/classes/WorkExperience.js
+++ src/component/classes/WorkExperience.js
@@ -9,6 +9,7 @@ Exemple :
         html 6
 */
 // ------------------------------------------------------------------------------------
+// Company and formation
 export class WorkCompany
 {
     constructor(job_title,summary,startdate,enddate,company,workcat) 
@@ -45,11 +46,12 @@ export class WorkCategory
 */
 export function generate_fake_data()
 {
-    let list_cat    = ['<i class="fa fa-code"></i> Languages dev',  '<i class="fa fa-book"></i> Framework / Bibliothèques', '<i class="fa fa-graduation-cap"></i> Experiences Professionnelles', 'Experiences Professionnelles hors développement']
+    let list_cat    = ['<i class="fa fa-code"></i> Languages dev',  '<i class="fa fa-book"></i> Framework / Bibliothèques', '<i class="fa fa-industry"></i> Experiences Professionnelles', 'Experiences Professionnelles hors développement','<i class="fa fa-graduation-cap"></i>Formations']
     let cat_lang    = new WorkCategory(list_cat[0])
     let cat_fram    = new WorkCategory(list_cat[1])
     let cat_company_info = new WorkCategory(list_cat[2])
     let cat_company_OS = new WorkCategory(list_cat[3])
+    let cat_formation = new WorkCategory(list_cat[4])
     let array_exp   = []
     // ---------------------------------------
 
@@ -82,13 +84,17 @@ export function generate_fake_data()
     // cat_company // job_title,summary,startdate,enddate,company,workca
     array_exp.push(new WorkCompany("Developpeur Web (Acutellement)","Accompagnement digital pour la transformation numérique d'un réseau d'agence immobilière. BackEnd avec Django + API REST avec un Front sous EmberJs et un  Front d'administration sous Django, D3js et ReactJs",new Date('2017-12'),"actuel","Nitroserv",cat_company_info))
     array_exp.push(new WorkCompany("Developpeur Web","Développement de nouveaux modules pour un outil d’email marketing et maintenance applicative de cet outil",new Date('2017-07'),new Date('2017-11'),"ID Nova",cat_company_info))
-    array_exp.push(new WorkCompany("Developpeur Informatique (Alternance)","Développement de sites internets en utilisant divers Frameworks & CMS. Application mobile hybride, embarquée, native et desktop ainsi qu'une extension pour navigateur.",new Date('2016-06'),new Date('2016-06'),"L2C2",cat_company_info))
+    array_exp.push(new WorkCompany("Developpeur Informatique (Alternance)","Développement de sites internets en utilisant divers Frameworks & CMS. Application mobile hybride, embarquée, native et desktop ainsi qu'une extension pour navigateur.",new Date('2016-06'),new Date('2017-07'),"L2C2",cat_company_info))
     array_exp.push(new WorkCompany("Developpeur Informatique","Automatisation de tâches de supervision et de maintenance grâce à du javascript et à du langage « BATCH ». Amélioration de la visualisation des informations.",new Date('2016-04'),new Date('2016-05'),"Spie",cat_company_info))
     array_exp.push(new WorkCompany("Technicien d’exploitation et de supervision","Maintenance et supervision de sites de productions et serveurs. Support par téléphone pour le support bas niveau, et rédaction de notes pour le suivi.",new Date('2016-01'),new Date('2016-04'),"Spie",cat_company_info))
     array_exp.push(new WorkCompany("Développeur informatique (Stage)","Refonte d’une interface pour la gestion de parc de machines virtuelles pour l’intégrer dans un site essentiellement avec du JavaScript et avec la bibliothèque D3.js",new Date('2013-06'),new Date('2013-08'),"Vates",cat_company_info))
     // ---------------------------------------
     // cat_company_OS 
 
+    // ---------------------------------------
+    // cat_formation 
+    array_exp.push(new WorkCompany("BAC + 3 : Licence Professionnelle - Métier de l'informatique applique au web (MI-AW)","Analyse & programmation informatiqe",new Date('2013'),new Date('2014'),"IUT 2 de Grenoble",cat_formation))
+    array_exp.push(new WorkCompany("BAC + 2 : DUT informatique","Analyse & programmation informatiqe",new Date('2013'),new Date('2014'),"IUT 2 de Grenoble",cat_formation))
     /* --------------------------------------- */
     let array_cat = [cat_fram,cat_lang,cat_company_info]
     array_exp = sort_by_level(array_exp)
